d949d67c471225515c67e76123b377a6d16b4b12
Morgan Harmon <morganharmon@gmail.com>
2022-04-27 11:33:31 -0700

    Add cheese event listener

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const sodaConst = document.getElementById('soda');
+const cheeseConst = document.getElementById('cheese');
+
 
 
 sodaConst.addEventListener('click', () => {
     sodaConst.classList.toggle('picked');
+});
+
+cheeseConst.addEventListener('click', () => {
+    cheeseConst.classList.toggle('picked');
 });
