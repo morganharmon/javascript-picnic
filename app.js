fc7a5b4b4a2a9cdc609c42b55358ca3ed5427169
Morgan Harmon <morganharmon@gmail.com>
2022-04-27 11:43:36 -0700

    Add random picker button

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cheeseConst = document.getElementById('cheese');
 const grapesConst = document.getElementById('grapes');
 const breadConst = document.getElementById('bread');
 const watermelonConst = document.getElementById('watermelon');
+const buttonConst = document.getElementById('button');
+let arr = [sodaConst, cheeseConst, grapesConst, breadConst, watermelonConst];
 
 sodaConst.addEventListener('click', () => {
     sodaConst.classList.toggle('picked');
@@ -22,4 +24,12 @@ breadConst.addEventListener('click', () => {
 
 watermelonConst.addEventListener('click', () => {
     watermelonConst.classList.toggle('picked');
+});
+
+// -   Add a button that will pick a random item from your picnic basket and add it to your picnic
+// -   Add drag and drop to your items (hint: drag and drop are events just like clicking)
+// -   Keep track of how many items have been picked and only allow users to select at most 3 items
+
+buttonConst.addEventListener('click', () => {
+    arr[Math.floor(Math.random() * 5)].classList.toggle('picked');
 });
