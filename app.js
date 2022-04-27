1763550ed5c2e48c440de7c7488fe2fbc6af4d17
Morgan Harmon <morganharmon@gmail.com>
2022-04-27 11:34:29 -0700

    Add grapes event listener

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const sodaConst = document.getElementById('soda');
 const cheeseConst = document.getElementById('cheese');
-
+const grapesConst = document.getElementById('grapes');
 
 
 sodaConst.addEventListener('click', () => {
@@ -9,4 +9,8 @@ sodaConst.addEventListener('click', () => {
 
 cheeseConst.addEventListener('click', () => {
     cheeseConst.classList.toggle('picked');
+});
+
+grapesConst.addEventListener('click', () => {
+    grapesConst.classList.toggle('picked');
 });
